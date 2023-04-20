575549aa858dcd6a1f20a41f4438209ab409282f
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2023-04-20 18:36:23 +0200

    number puzzle

---
+++ examples/number_puzzles/bk.pl
@@ -0,0 +1,21 @@
+
+plus(X,Y,Z) :-
+        integer(X),
+        integer(Y),
+        Z is X + Y.
+
+minus(X,Y,Z) :-
+        integer(X),
+        integer(Y),
+        Z is X - Y.
+
+mult(X,Y,Z) :-
+        integer(X),
+        integer(Y),
+        Z is X * Y.
+
+div(X,Y,Z) :-
+        integer(X),
+        integer(Y),
+        Y > 0,
+        Z is X / Y.
